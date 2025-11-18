48e96c92f8cc42f6523dc1e88009c28aa5402227
Noah Christensen <noah.d.christensen@gmail.com>
2025-11-18 13:03:07 -0600

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import spa from 'vite-plugin-spa';
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [react(), spa()],
+  build: {
+    rollupOptions: {
+      output: {
+        // ensures single-page app fallback works
+      }
+    }
+  }
 })
 
