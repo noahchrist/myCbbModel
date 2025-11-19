121ec41e725b46c95f5074d4c91dedadf6a7dbcf
Noah Christensen <noah.d.christensen@gmail.com>
2025-11-18 18:59:33 -0600

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,9 +25,15 @@ if SUPABASE_SERVICE_ROLE_KEY:
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "https://weightroom.io",
+    "https://www.weightroom.io",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
